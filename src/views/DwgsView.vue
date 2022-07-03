f6757d359b3641c3210fab7ed80b515a532624c6
Pablo <pablogaudard@gmail.com>
2022-07-03 17:30:18 -0300

    ++

--- src/views/DwgsView.vue
+++ src/views/DwgsView.vue
@@ -1,6 +1,15 @@
 <template>
   <div id="dwgs">
     <router-link class="btn-voltar" to="/"></router-link>
+
+
+
+    <router-link
+    v-for="i in 10" v-bind:key="i"
+    style="width: 11.2%; height: 16%;"
+    :style="{left: `${8 + ((i<=5?i:i-5)-1) * 17.23}%`, top: `${(i<=5?24.5:57)}%`}"
+    :to="'/dwg/'+i"></router-link>
+    
   </div>
 </template>
 
@@ -13,7 +22,7 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #dwgs {
   background-image: url(../assets/dwgs.png);
   background-size: contain;
