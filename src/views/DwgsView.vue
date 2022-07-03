ea2008d8ca9ff3eda9c2fa9522553a1daf7a477b
Pablo <pablogaudard@gmail.com>
2022-07-03 18:00:56 -0300

    ++

--- src/views/DwgsView.vue
+++ src/views/DwgsView.vue
@@ -2,16 +2,12 @@
   <div id="dwgs">
     <router-link class="btn-voltar" to="/"></router-link>
 
-      <iframe v-if="$route.params.file"
-      src="./dwgs/ATICO COTADO.pdf"
-      width="100%" height="100%" style="border:none"></iframe> 
+    <iframe v-if="$route.params.file" src="../dwgs/ATICO COTADO.pdf" width="100%" height="100%"
+      style="border:none"></iframe>
 
-    <router-link
-    v-for="i in 10" v-bind:key="i"
-    style="width: 11.2%; height: 16%;"
-    :style="{left: `${8 + ((i<=5?i:i-5)-1) * 17.23}%`, top: `${(i<=5?24.5:57)}%`}"
-    :to="'/dwg/'+i"></router-link>
-    
+    <router-link v-for="i in 10" v-bind:key="i" style="width: 11.2%; height: 16%;"
+      :style="{ left: `${8 + ((i <= 5 ? i : i - 5) - 1) * 17.23}%`, top: `${(i <= 5 ? 24.5 : 57)}%` }" :to="'/dwgs/' + i"></router-link>
+
   </div>
 </template>
 
