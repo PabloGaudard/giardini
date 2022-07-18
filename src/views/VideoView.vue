8fa8d1c64712dbf2d0ce3092781d4ec7730df63e
Pablo <pablogaudard@gmail.com>
2022-07-18 19:11:38 -0300

    ++

--- src/views/VideoView.vue
+++ src/views/VideoView.vue
@@ -1,5 +1,11 @@
 <template>
   <div id="video">
+
+    <video controls autoplay>
+      <source :src="require('@/assets/Giardini_MB_alta.mp4')" type="video/mp4">
+      Your browser does not support the video tag.
+    </video>
+
     <router-link class="btn-voltar" to="/"></router-link>
   </div>
 </template>
@@ -13,9 +19,16 @@ export default {
 }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #video {
-  background-image: url(../assets/video.png);
+///  background-image: url(../assets/video.png);
+
+  video{
+    top: 20%;
+    left: 18%;
+    width: 64%;
+    height: 65%;
+  }
 
 }
 </style>
